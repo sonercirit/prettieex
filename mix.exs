3ea7f3d5b81f351df413dfbcfd2ca873cdb40750
sonercirit <sonercirit@gmail.com>
2020-04-03 14:12:14 +0300

    adds floki for html parsing

--- mix.exs
+++ mix.exs
@@ -23,7 +23,8 @@ defmodule EexFormatter.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:credo, "~> 1.3", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.3", only: [:dev, :test], runtime: false},
+      {:floki, "~> 0.26.0"}
     ]
   end
 end
