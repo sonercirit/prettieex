9be19b8713ec725179317b24a9d396d93101f965
sonercirit <sonercirit@gmail.com>
2020-04-06 13:26:13 +0300

    add async execution per file with default value

--- lib/prettieex.ex
+++ lib/prettieex.ex
@@ -3,8 +3,28 @@ defmodule Mix.Tasks.Prettieex do
   use Mix.Task
 
   def run(args) do
-    [path | _] = args
-    abspath = path |> Path.expand()
-    Path.wildcard(abspath) |> Enum.each(fn file -> file |> EExFormatter.process_file() end)
+    path = args |> List.first()
+
+    path =
+      if path === nil do
+        "lib/**/*.html.eex"
+      else
+        path
+      end
+
+    path
+    |> Path.expand()
+    |> Path.wildcard()
+    |> Enum.reduce([], fn file, acc ->
+      [
+        Task.async(fn ->
+          file |> EExFormatter.process_file()
+        end)
+        | acc
+      ]
+    end)
+    |> Enum.each(fn task ->
+      task |> Task.await()
+    end)
   end
 end
